9a0c757ed0301bb6637f0834d88e2e54bfaec6d8
plijy8 <barmotin8@yandex.ru>
2016-06-15 08:10:40 +0300

    12

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,13 +1,51 @@
 $(document).ready(function(){
     
+var touch = $('#touch-menu');
+       var menu = $('.mainMenu');
+       var width = $(window).width();
+        var first = true;
+
+
+
 //плавный переход по ссылкам
         
        $('a[href^="#"]').click(function(){
         var el = $(this).attr('href');
-        $('body').animate({
-            scrollTop: $(el).offset().top-69}, 1500);
+        if(width>787)
+        {
+            $('html, body').animate({
+            scrollTop: $(el).offset().top-69}, 1500);    
+        }
+        else{
+            $('html, body').animate({
+            scrollTop: $(el).offset().top}, 1500);
+        }
         return false; 
+
 });
+
+
+
+            $(touch).on('click', function(e) {
+                e.preventDefault();
+                menu.slideToggle();
+                if(first)
+                {
+                    touch.addClass("activeMenu");
+                    first = false;    
+                }
+                else{
+                    touch.removeClass("activeMenu");
+                    first = true; 
+                }
+                
+            });
+            $(window).resize(function(){
+                var wid = $(window).width();
+                if(wid > 787 && menu.is(':hidden')) {
+                    menu.removeAttr('style');
+                }
+        });
 
        //липкое меню
 
@@ -17,12 +55,16 @@ $(document).ready(function(){
         $(window).scroll(function(){
             if( $(window).scrollTop() > HeaderTop ) {
                 $('#mainMenu').addClass("mainMenuWrap") ;
-                $(".mainMenu__item > a").removeClass('mainMenu__item_white') ;
+                if(width>787){
+                    $(".mainMenu__item > a").removeClass('mainMenu__item_white') ;    
+                }
             } 
             else {
                $('#mainMenu').removeClass("mainMenuWrap");
-               $(".mainMenu__item > a").addClass('mainMenu__item_white');
-            
+               if(width>787){
+                    $(".mainMenu__item > a").addClass('mainMenu__item_white'); 
+               }
+                          
             }
             if( ($(window).scrollTop() ) > HeaderTop ) {
                 $('.topLink').css({visibility: 'visible'}); 
